2ba08f79c5188fb1866513e98b634e7602681ec5
unprobable-x <bryant.graham@gmail.com>
2022-05-30 11:40:46 -0400

    hover functionality working

---
+++ script.js
@@ -0,0 +1,21 @@
+const container = document.getElementById("container");
+
+function makeRows(rows, cols) {
+  container.style.setProperty('--grid-rows', rows);
+  container.style.setProperty('--grid-cols', cols);
+  for (c = 0; c < (rows * cols); c++) {
+    let cell = document.createElement("div");
+    //cell.innerText = (c + 1);
+    container.appendChild(cell).className = "grid-item";
+  };
+};
+
+container.addEventListener('mouseover', function (e) {
+    // Add the "active" class to only divs with a "box" class
+    if (e.target.matches('.grid-item')) {
+      e.target.classList.add('active');
+    }
+  })
+
+makeRows(4, 4);
+
